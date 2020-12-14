297664f47e375a0545bd380bcd4bf4a3932dc329
Fernet97 <pierluigi.liguori97@gmail.com>
2020-12-14 14:35:45 +0100

    fix bug

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+
 
 
 app.get('/', function(req, res) {
   console.log("BBBBBBBBBBBBBHHHHH u serverin ;)");
+  res.send('<html><body><h1>BBBBBBBBBBBBBHHHHH u serverin ;)</h1></body></html>');
 });
 
 
 
 app.get('/prova', function(req, res) {
   console.log("qualcuno si è connesso a prova");
+  res.send('<html><body><h1>BBBBBBBBBBBBBHHHHH</h1></body></html>');
 });
 
 
